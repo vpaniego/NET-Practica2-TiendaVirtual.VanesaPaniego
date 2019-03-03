cd07171f60cd8e88cd55665f1bf58639084d94f0
Paniego Seco <vanesa.paniego@fujitsu.com>
2019-03-03 10:24:45 +0100

    Agregar archivos de proyecto.

---
+++ TiendaVirtual_CarritoCompra/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TiendaVirtual_CarritoCompra.MvcApplication" Language="C#" %>
---
+++ TiendaVirtual_CarritoCompra/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TiendaVirtual_CarritoCompra.Startup))]
+namespace TiendaVirtual_CarritoCompra
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
