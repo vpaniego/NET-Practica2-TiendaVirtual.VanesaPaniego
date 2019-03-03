1535ae5d18e6ae9b055e39df90019b90b22f1c1b
vpaniego <vanesa.paniegos@alumnos.upm.es>
2019-03-03 12:26:58 +0100

    MVC + Model Binders para las entidades Categorias y Productos

---
+++ TiendaVirtual_CarritoCompra/Models/Binders/CategoriasModelBinder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TiendaVirtual_CarritoCompra.Models.Binders
+{
+    public class CategoriasModelBinder : IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            HttpSessionStateBase session = controllerContext.HttpContext.Session;
+            Categorias categoria = (Categorias) session["KEY_CATEGORIA"];
+            if(categoria == null)
+            {
+                categoria = new Categorias();
+                session["KEY_CATEGORIA"] = categoria;
+            }
+
+            return categoria;
+        }
+    }
+}
---
+++ TiendaVirtual_CarritoCompra/Models/Binders/ProductosModelBinder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TiendaVirtual_CarritoCompra.Models.Binders
+{
+    public class ProductosModelBinder : IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            HttpSessionStateBase session = controllerContext.HttpContext.Session;
+            Productos producto = (Productos)session["KEY_PRODUCTO"];
+            if(producto == null)
+            {
+                producto = new Productos();
+                session["KEY_PRODUCTO"] = producto;
+            }
+
+            return producto;
+        }
+    }
+}
